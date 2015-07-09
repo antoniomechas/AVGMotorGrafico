e97a8afc9c12a47a0847ca8cff6e62ea0a94f693
antoniomechas <antoniomechas@gmail.com>
2015-07-09 11:28:27 +0200

    first

---
+++ AVGMotorGrafico.cpp
@@ -0,0 +1,562 @@
+//2
+//3 - desde el PC, en el servidor tiene que haber un //5 desde el portatil
+// desde el portatil
+#include "AVGMotorGrafico.h"
+
+void AVGMotorGrafico::setup(int w, int h)
+{
+	width = w;
+	height = h;
+
+	fbo.allocate(w, h, GL_RGBA32F);
+	fboTex.allocate(w, h, GL_RGBA32F);
+	fbo2.allocate(w, h, GL_RGBA32F);
+	fboOut.allocate(w, h, GL_RGBA32F);
+	
+//	texMask.allocate(w, h, GL_LUMINANCE);
+
+	//imagenOrigen.allocate(w,h);
+	//imagenDestino.allocate(w,h);
+	//imagenFondo.allocate(w,h);
+	//textura.allocate(w,h);
+	//imagenTmp.allocate(w,h);
+//	imgTmp.allocate(w,h,OF_IMAGE_COLOR);
+
+//	tOrigen = ORIGEN_CAPTURA_DRAW;
+	//bUsaShader = true;
+	//bOn = false; //Esta desactivado por defecto
+	//bClipping = true;
+	//bUsaImgFondo = false;
+	//iShader = 0;
+
+	//loadImage(&imagenFondo, "data/images/tanque.jpg");
+	//loadImage(&textura, "shaders/texturas/tex01.jpg");
+
+	iIntervaloMax = 0;
+	iIntervaloMin = 0;
+	lastTimeChecked = ofGetElapsedTimeMillis();
+	lastTimeInterval = ofGetElapsedTimeMillis();
+
+	shaderI.load("shaders/invertir");		//  Se carga el shader que se encarga de invertir
+
+//	maskType = MASK_TYPE_IMG;	// Para darle un valor por defecto
+
+	setupPingPong();
+
+	//Parametros del contorno que se le pasará al shader
+	shaderCenter = ofVec2f(0.5, 0.5);
+	shaderWidth = 0.5;
+	shaderHeight = 0.5;
+
+	bDontDraw = false;
+
+	//ofxPostProcessing
+	post.init(w, h);
+	post.createPass<BloomPass>()->setEnabled(false);
+    post.createPass<FxaaPass>()->setEnabled(false);
+
+	bClipping = false; //esto ya no se utiliza con la llegada del resolume
+}
+
+
+
+void AVGMotorGrafico::setupPingPong( void )
+{
+	ofEnableArbTex();
+	//pingPong[0].allocate(width, height, GL_RGB);
+	//pingPong[1].allocate(width, height, GL_RGB);
+	pingPong[0].allocate(width, height, GL_RGBA32F_ARB);
+	pingPong[1].allocate(width, height, GL_RGBA32F_ARB);
+	//pingPong[0].allocate(width, height, GL_RGBA);
+	//pingPong[1].allocate(width, height, GL_RGBA);
+    for(int i = 0; i < 2; i++)
+	{
+		pingPong[i].begin();
+			ofClear(0,255);
+        pingPong[i].end();
+	}
+
+	src = &pingPong[0];
+	dst = &pingPong[1];
+
+	cout << endl << "Cargando shader damping / alpha" << endl;
+	shaderPingPong.load("shaders/pintura");
+}
+
+
+void AVGMotorGrafico::draw( void )
+{
+	// Si no está activo, no tiene nada que hacer
+	if (!bOn)
+		return;
+
+	ofPushStyle();
+
+	if (bUsaShader)
+	{
+		//  Lo hará cada MILIS_CHECK
+		if (ofGetElapsedTimeMillis() - lastTimeChecked > MILIS_CHECK)
+		{
+			setNombreShader();
+			string frag = "shaders/" + nombreShader + ".frag";
+			string vert = "shaders/" + nombreShader + ".vert";
+
+			ofFile fragFile(frag), vertFile(vert);
+			Poco::Timestamp fragTimestamp = fragFile.getPocoFile().getLastModified();
+			Poco::Timestamp vertTimestamp = vertFile.getPocoFile().getLastModified();
+			if(fragTimestamp != lastFragTimestamp || vertTimestamp != lastVertTimestamp)
+			{
+				shader1.load("shaders/" + nombreShader);
+				//shader2.load("shaders/shader2");
+			}
+
+			lastFragTimestamp = fragTimestamp;
+			lastVertTimestamp = vertTimestamp;
+		}
+	}
+
+	//if (tOrigen == ORIGEN_IMAGEN) //  si el modo es capturaDraw, fboTex ya tendrá capturado el draw
+	//{
+	//
+	//	ofPushStyle();
+	//	ofPushMatrix();
+	//	fboTex.begin();
+	//        ofClear(0,255);
+	//		imagenOrigen.draw(0,0);
+	//	fboTex.end();
+	//    ofPopMatrix();
+ //       ofPopStyle();	
+
+	//}		
+	
+	// En este punto, fboTex contiene la captura del draw
+	// puede ser la copia de src, en el caso de que se esté utilizando Ping Pong
+
+	ofDisableAlphaBlending();
+	//fbo.begin();
+	//
+	//	if (bUsaShader)
+	//		drawShader();
+	//	else	
+	//		fboTex.draw(0,0);		//  Dibujamos la imagen de origen, ya que no se utiliza el shader
+
+	//fbo.end();
+
+	
+	if (bUsaShader)
+	{
+		fbo.begin();
+			drawShader();
+		fbo.end();
+	}
+	else	
+		fbo = fboTex;
+
+	// En fbo tenemos el resultado hasta ahora de la imagen resultante
+	// en el caso de que el ping pong esté habilitado, lo utilizamos sobre el fbo actual
+	// el shader ping pong le aplicara un blur segun el parámetro kernelSize, y un alpha damping
+	// TODO: incluir el invertir en este shader también, para matar los 3 pajaros en un shader
+	if (bUsePingPong)
+		drawPingPong();
+
+	if (bInvertir && !bFlash)
+		bDoInvertir = true;
+
+	if (!bInvertir && !bFlash)
+		bDoInvertir = false;
+
+	if (bFlash)
+	{
+		if ((iIntervaloMin == 0) && (iIntervaloMax == 0))
+			bDoInvertir = true;
+		else
+		{
+			if ((ofGetElapsedTimeMillis() - lastTimeInterval) > ofRandom(iIntervaloMin,iIntervaloMax))
+			{
+				bDoInvertir = !bDoInvertir;
+				lastTimeInterval = ofGetElapsedTimeMillis();
+			}
+		}
+	}
+
+	fbo2.begin();
+
+		if (bDoInvertir)
+		{
+			shaderI.begin();
+			shaderI.setUniformTexture("tex0", fbo.getTextureReference(),  1);
+			renderDummy();
+			shaderI.end();
+		}
+		else
+		{
+			fbo.draw(0,0);
+		}
+
+	fbo2.end();
+
+	if (bDontDraw)
+		fboOut = fbo2;
+	else
+	{
+		if (bPostBloom || bPostFxaa)
+		{
+			post[0]->setEnabled(bPostBloom);
+			post[1]->setEnabled(bPostFxaa);
+			post.begin();
+				fbo2.draw(0,0,width,height);
+			post.end();
+		}
+		else
+		{
+			fbo2.draw(0,0,width,height);
+		}
+	}
+
+	if (bClipping)
+		drawClipping();
+
+	ofPopStyle();
+
+}
+
+//
+//	Dibuja en el FBO de ping pong para hacerle un blur y alpha damping y mantener el resultado
+//
+void AVGMotorGrafico::drawPingPong()
+{
+	ofSetColor(255,255,255);
+
+	//enableSelectedBlending();
+	
+	//src = fbo;
+	
+	//fbo contiene src, por lo que se utiliza 
+
+	//src->begin();
+	//	fbo.draw();
+	//src->end();	
+	//ofDisableAlphaBlending();
+	
+	//ofEnableAlphaBlending();
+	//ofEnableBlendMode(OF_BLENDMODE_ADD);
+	
+	//glBlendFuncSeparate(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA,GL_ONE,GL_ONE_MINUS_SRC_ALPHA); 
+
+	ofDisableAlphaBlending();
+	
+	dst->begin();
+		//ofClear(0, 0, 0, 0);
+		shaderPingPong.begin();
+			shaderPingPong.setUniformTexture("tex0", fbo.getTextureReference(), 1);
+			shaderPingPong.setUniform1i("kernelSize", iKernelSize);
+			shaderPingPong.setUniform1f("alphaDamping", fPingPongAlphaDamping);
+			//shader.setUniform1i("velX", paramVelX);
+			//shader.setUniform1i("velY", paramVelY);
+			renderDummy();
+		shaderPingPong.end();
+	dst->end();
+
+	//ofEnableAlphaBlending();
+	//glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	//dst->begin();
+	//	//ofClear(0, 0, 0, 0);
+	//	fbo.draw(0,0);
+	//dst->end();
+
+
+	swap(src, dst);
+	
+	//ofEnableBlendMode(OF_BLENDMODE_ALPHA);
+	
+	//ofDisableAlphaBlending();
+	
+
+	fbo.begin();
+		//ofClear(0, 0, 0, 0);
+		src->draw(0,0);
+	fbo.end();
+
+	//fbo = *src;
+
+	//ofDisableAlphaBlending();
+
+}
+
+//
+//	Dibuja la escena con el shader correspondiente
+//
+void AVGMotorGrafico::drawShader()
+{
+	shader1.begin();
+
+		shader1.setUniform2f("resolution",width, height);
+		//shader1.setUniform2f("mouse",(float)iShaderX / (float)width, (float)iShaderY / (float)height);
+		shader1.setUniform1f("time", ofGetElapsedTimef() );
+		//shader1.setUniform1f("volumen", volumen );
+		shader1.setUniform1f("alphaShader", fAlphaShader );
+		
+		switch(iShader)
+		{
+			case 1:	// Tunnel
+				//cout << "centro: " << shaderCenter << endl;
+				shader1.setUniform2f("centro", shaderCenter.x, shaderCenter.y);
+				shader1.setUniform1f("width", shaderWidth);
+				shader1.setUniform1f("height", shaderHeight);
+				break;
+
+			case 2: //flare
+				shader1.setUniform2f("center", shaderCenter.x, shaderCenter.y);
+				break;
+			
+			case 3:
+				//textura.draw(0,0);
+				shader1.setUniform4f("iMouse",shaderCenter.x, shaderCenter.y, 1.0f, 1.0f);
+				shader1.setUniformTexture("tex0", fboTex.getTextureReference(),  1);
+				break;
+
+			case 4: //voronoi
+				//cout << ofNoise(ofGetElapsedTimef()) << endl;
+				shader1.setUniformTexture("tex0", fboTex.getTextureReference(),  1);
+				shader1.setUniform1f("density", 120.0f * ofNoise(ofGetElapsedTimef()));
+				break;
+
+			case 5: //blur with mask
+				//fboTex.draw(0,0);
+				shader1.setUniform1i("kernelSize", iKernelSize);
+				shader1.setUniformTexture("tex0", fboTex.getTextureReference(),  1);
+				//if (maskType == MASK_TYPE_IMG)
+				//	shader1.setUniformTexture("tex1", texMask,  2);
+				//else
+				//	shader1.setUniformTexture("tex1", *texMaskExterna,  2);
+				break;
+
+			default:
+				shader1.setUniform4f("iMouse",shaderCenter.x, shaderCenter.y, 1.0f, 1.0f);
+				shader1.setUniformTexture("tex0", fboTex.getTextureReference(),  1);
+		}
+		
+		renderDummy();	//Para que el shader se active	
+	
+	shader1.end();
+
+}
+
+
+//
+//  Utiliza la imagen de fondo como base y le aplica efectos según lo que hay en imagenDestino
+//
+//void AVGMotorGrafico::aplicaEfectoSobreFondo()
+//{
+//	int p = 0;
+//	float f;
+//	unsigned char *c = imagenFondo.getPixels();
+//	unsigned char *d = imagenDestino.getPixels();
+//	//voy a suponer que los pixeles estan en el rango 0-255
+//	/*
+//	for (int i = 0 ; i < imagenFondo.width ; i++)
+//		for (int k = 0 ; k < imagenFondo.height ; k++)
+//		{
+//			for (int h = 0; h < 3; h++)
+//			{
+//				f = (float)imagenFondo.getPixels()[p] * ((float)imagenDestino.getPixels()[p] / 255.0f);
+//				imagenDestino.getPixels()[p] = (unsigned char)f;
+//				p++;
+//
+//			}
+//		}
+//		//cvAnd(imagenDestino.getCvImage(), imagenFondo.getCvImage(), imagenTmp.getCvImage());
+//	*/
+//	for (int i = 0 ; i < imagenFondo.width * imagenFondo.height * 3 ; i++)
+//	{
+//		f = (float)c[i] * ((float)d[i] / 255.0f);
+//		//f = c[i];
+//		//d[i] = c[i];
+//		d[i] = (unsigned char)f;
+//	}
+//
+//	//imagenDestino = imagenTmp;
+//}
+
+
+void AVGMotorGrafico::drawClipping()
+{
+	//Clipping
+	//ofSetColor(0,0,0);
+	glEnable(GL_BLEND); 
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable( GL_LINE_SMOOTH );
+	glEnable( GL_POLYGON_SMOOTH );
+	glHint( GL_LINE_SMOOTH_HINT, GL_NICEST );
+	glHint( GL_POLYGON_SMOOTH_HINT, GL_NICEST );
+
+	glColor3f(0.0f,0.0f,0.0f);
+	ofFill();
+	
+	glBegin(GL_QUADS);
+		glVertex2f(0,0);
+		glVertex2f(ptoClipping[0].x, ptoClipping[0].y);
+		glVertex2f(ptoClipping[3].x, ptoClipping[3].y);
+		glVertex2f(0,height);
+	glEnd();
+
+	glBegin(GL_QUADS);
+		glVertex2f(0,0);
+		glVertex2f(width,0);
+		glVertex2f(ptoClipping[1].x, ptoClipping[1].y);
+		glVertex2f(ptoClipping[0].x, ptoClipping[0].y);
+	glEnd();
+
+	glBegin(GL_QUADS);
+		glVertex2f(ptoClipping[1].x, ptoClipping[1].y);
+		glVertex2f(width,0);
+		glVertex2f(width,height);
+		glVertex2f(ptoClipping[2].x, ptoClipping[2].y);
+	glEnd();
+
+	glBegin(GL_QUADS);
+		glVertex2f(ptoClipping[3].x, ptoClipping[3].y);
+		glVertex2f(ptoClipping[2].x, ptoClipping[2].y);
+		glVertex2f(width,height);
+		glVertex2f(0,height);
+	glEnd();
+
+	ofNoFill();
+	
+	glDisable( GL_LINE_SMOOTH );
+	glDisable( GL_POLYGON_SMOOTH );
+	glDisable( GL_BLEND );
+}
+
+//void AVGMotorGrafico::update( ofxCvGrayscaleImage *img )
+//{
+//
+//	imagenOrigen = *(img);
+//
+//}
+
+//void AVGMotorGrafico::setImagenOrigen( tipoOrigen t)
+//{
+//	tOrigen = t;
+//}
+
+// OJO: si en el gui se cambia que haya o no ping pong justo entre captura.begin y captura.end -> incongruencia
+void AVGMotorGrafico::beginCaptureDraw()
+{
+	if (!bOn)
+		return;
+
+	ofPushStyle();
+	ofPushMatrix();
+	if (bUsePingPong)
+		src->begin();
+	else
+		fboTex.begin();
+	ofClear(0,0);
+}
+
+void AVGMotorGrafico::endCaptureDraw()
+{
+	if (!bOn)
+		return;
+	
+	if (bUsePingPong)
+	{
+		src->end();
+		// copio src en fboTex para que utilice siempre este FBO en los shaders a continuación
+		//fboTex = *src;
+		ofEnableAlphaBlending();
+		//ofEnableBlendMode(OF_BLENDMODE_ALPHA);
+		fboTex.begin();
+			src->draw(0,0);
+		fboTex.end();
+		ofDisableAlphaBlending();
+	}
+	else
+		fboTex.end();
+
+	ofPopMatrix();
+    ofPopStyle();	
+}
+
+void AVGMotorGrafico::renderDummy()
+{
+	ofSetColor(255,255,255);
+	glBegin(GL_QUADS);
+    //glTexCoord2f(0, 0); glVertex3f(0, 0, 0);
+    //glTexCoord2f(width, 0); glVertex3f(width, 0, 0);
+    //glTexCoord2f(width, height); glVertex3f(width, height, 0);
+    //glTexCoord2f(0,height);  glVertex3f(0,height, 0);
+    glTexCoord2f(0, 0); glVertex3f(0, 0, 0);
+    glTexCoord2f(1.0f, 0); glVertex3f(width, 0, 0);
+    glTexCoord2f(1.0f, 1.0f); glVertex3f(width, height, 0);
+    glTexCoord2f(0,1.0f);  glVertex3f(0,height, 0);
+    glEnd();
+}
+
+void AVGMotorGrafico::setNombreShader()
+{
+	switch (iShader)
+	{
+		case 0:
+			nombreShader = "generaColor";
+			break;
+
+		case 1:
+			nombreShader = "tunnel_purple";
+			break;
+
+		case 2:
+			nombreShader = "flare";
+			break;
+
+		case 3:
+			nombreShader = "emboss";
+			break;
+
+		case 4:
+			nombreShader = "voronoi";
+			break;
+
+		case 5:
+			nombreShader = "GaussianBlur";
+			break;
+
+	}
+
+}
+
+
+//Carga una imagen para utilizarla como fondo
+//void AVGMotorGrafico::loadImage(ofxCvColorImage *img, const char* archivo)
+//{
+//
+//	ofImage fileImage;
+//	
+//	fileImage.loadImage(archivo);
+//	fileImage.resize(width, height);
+//
+//	*img = fileImage.getPixels();
+//
+//}
+
+
+//  Establece la textura que se utilizará como entrada de audio
+void AVGMotorGrafico::setAudioTexture( ofTexture tex )
+{
+
+	texAudio = &tex;
+
+}
+
+//void AVGMotorGrafico::setMaskTexture( ofxCvGrayscaleImage img )
+//{
+//	maskType = MASK_TYPE_IMG;
+//	texMask.loadData(img.getPixelsRef());
+//}
+//
+//void AVGMotorGrafico::setMaskTexture( ofTexture *tex )
+//{
+//	maskType = MASK_TYPE_TEX;
+//	texMaskExterna = tex;
+//}
---
+++ AVGMotorGrafico.h
@@ -0,0 +1,113 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxOpenCv.h"
+#include "ofxGui.h"
+#include "ofxPostProcessing.h"
+#include "ofxFilterLibrary.h"
+
+#define MILIS_CHECK 1000			// Mirará si el shader ha cambiado cada 1000 milis = 1 seg
+//#define MASK_TYPE_IMG 0
+//#define MASK_TYPE_TEX 1
+
+class AVGMotorGrafico
+{
+	
+	public:
+		
+		//enum tipoOrigen {ORIGEN_IMAGEN, ORIGEN_CAPTURA_DRAW};
+
+		void		draw();
+		void		setup( int w, int h);
+		void		update( ofxCvGrayscaleImage *img);
+		//void		setImagenOrigen( tipoOrigen t);
+		void		beginCaptureDraw();
+		void		endCaptureDraw();
+		void		setAudioTexture( ofTexture tex );									// Establece la textura que se utilizará como entrada de audio
+		//void		setMaskTexture( ofxCvGrayscaleImage img);							// le pasa una imagen parar guardar la textura
+		//void		setMaskTexture( ofTexture *tex);									// le pasa una textura externa para que la utilice como mascara
+
+		ofxToggle		bUsaShader;				//  Dibujará utilizando el shader
+		ofxToggle		bOn;					//  si está activo o no
+		ofxToggle		bClipping;				//  si se hace clipping
+		ofxToggle		bInvertir;				//  si invierte la imagen antes de dibujarla
+		ofxToggle		bDontDraw;				//  No dibuja, guarda el fbo para que se utilice después
+		ofxIntSlider	iShader;				//  shader a utilizar
+		ofxIntSlider	iKernelSize;			//  Para el gaussian blur shader
+		ofxToggle		bFlash;					//  Establece el modo Flash (parpadeo, cambia entre invertido y no invertido). Independiente del valor de bInvertir
+		ofxIntSlider	iIntervaloMin;			//  intervarlo en milisegundos para cambiar entre invertido o no
+		ofxIntSlider	iIntervaloMax;			//  intervarlo en milisegundos para cambiar entre invertido o no
+		ofxToggle		bUsePingPong;			//  Establece si utiliza el shader con un FBO ping pong, para hacer un blur, guardar el estado y hacer alpha damping
+		ofxFloatSlider	fPingPongAlphaDamping;	//  Valor de alpha damping para el shader de ping pong [0...1]	
+		ofxToggle		bPostBloom;				//  ofxPostProcessing
+		ofxToggle		bPostFxaa;				//  ofxPostProcessing
+
+		ofVec2f			shaderCenter;			//  Centro del objeto del shader
+		float			shaderHeight;			//  Alto del objeto
+		float			shaderWidth;			//	Ancho del objeto
+
+		//bool			bUsaImgFondo;			//  se utilizará la imagen cargada como fondo
+
+		float			fAlphaShader;			//  Entre 0...1 -> se le suma en el shader al R+G+B, para aumentar el brillo
+
+		CvPoint			ptoClipping[4];			//  define el area de clipping, lo lee desde el fichero de configuración el padre
+
+		ofTexture		*texAudio;				//  se establecerá desde setAudioTexture() para que sirva de input a los shaders que trabajan con sonido
+		//ofTexture		texMask;				//  Para los shaders que necesitan mascara
+		//ofTexture		*texMaskExterna;		//  Para los shaders que necesitan mascara, se establece con setMaskTexture()
+		float			volumen;
+
+		ofFbo			fboOut;					// Se utliza cuando no se quiere dibujar en pantalla, sino en un fbo para reutilizarlo después
+
+
+	private:
+		
+		void				renderDummy();
+		void				drawClipping();														//  Recorta la imagen
+		void				setNombreShader();													//  Rellena la variable nombreShader con el nombre del shader escogido con iShader.
+		//void				loadImage(ofxCvColorImage *img, const char* archivo);				//  Carga una imagen desde el disco
+		//void				aplicaEfectoSobreFondo();											//  combina la imagen de fondo con la que hay que dibujar para crear efectos sobre la imagen de fondo
+		void				drawShader();														//  Dibuja lo que toque con el shader especificado
+		void				setupPingPong();													//  Inicializa el shader y los FBOs para el ping pong
+		void				drawPingPong();														//  Dibuja sobre el fbo de ping pong para aplicarle el shader de blur y alpha damping
+
+		string				nombreShader;
+
+		Poco::Timestamp		lastFragTimestamp, lastVertTimestamp;								//  Para saber si hace falta recargar el archivo de shader
+
+		ofShader			shader1;		
+		//ofShader			shader2;
+		ofShader			shaderI;
+		ofFbo				fbo;
+		ofFbo				fbo2;
+		ofFbo				fboTex;
+
+		//ofxCvGrayscaleImage	imagenOrigen;
+		//ofxCvColorImage		imagenDestino;		//  Se utilizará para dibujar
+		//ofxCvColorImage     imagenFondo;		//  Se carga de disco para utilizarla como fondo o para darle efectos
+		//ofxCvColorImage     imagenTmp;			//  para calculos intermedios
+		//ofxCvColorImage     textura;			//  textura para pasársela al shader
+
+		//ofImage				imgTmp;
+
+		int					width;
+		int					height;
+
+		//tipoOrigen			tOrigen;			//  imagen : Dibujará la imagen que se le pase en update() con los procesos especificados
+												//  CapturaDraw: Capturará el draw() con begin() y end(), y lo guardará en una textura para utilizarlo en el shader
+		
+		unsigned long long	lastTimeChecked;	//  guarda la última vez que se miró si el archivo del shader se había actualizado
+		unsigned long long	lastTimeInterval;	//  guarda la última vez que se cambió de un estado a otro según el intervalo (de invertido a no)
+		bool				bDoInvertir;			//  se pondrá a true si hay que invertir, porque se ha marcado en el gui o porque toca por intervalo
+		
+		//int					maskType;			// 0 = mascara es texMask, 1 = mascara es un referencia a una textura externa en texMaskExterna
+
+		ofxPostProcessing	post;
+
+		// Para el fbo de pingPong
+		ofFbo					pingPong[2];
+		ofFbo					*src;
+		ofFbo					*dst;
+		ofShader				shaderPingPong;
+
+};
